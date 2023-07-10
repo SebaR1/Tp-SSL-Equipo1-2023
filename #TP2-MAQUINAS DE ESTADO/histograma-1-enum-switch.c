0097c467f34d888ac973100b914a5ae47ad1afac
JulianTettamanti <jtettamanti@frba.utn.edu.ar>
2023-07-10 12:29:36 -0300

    Primer implementación

--- #TP2-MAQUINAS DE ESTADO/histograma-1-enum-switch.c
+++ #TP2-MAQUINAS DE ESTADO/histograma-1-enum-switch.c
@@ -1,45 +1,47 @@
 #include <stdio.h>
 
-int main (){
-
-
+int main ()
+{
     {
         typedef enum {IN,OUT} State;
         State state = OUT;
         int v[999];
-        int cc;
-        int c=0;
+        int c=0,i=0,cc;
        
-        while ( c = getchar() != EOF ){
+        while ( c = getchar() != EOF )
+        {
             switch (state)
-            {
-            case OUT:
-                switch (c) //Si esta en OUT y recibe un caracter:
                 {
-                case (" "||"\n"||"\t"):   //Se queda en OUT
+                case OUT:
+                    switch (c) //Si esta en OUT y recibe un caracter:
+                    {
+                    case (" "||"\n"||"\t"):   //Se queda en OUT
+                        break;
+
+                    default:   //Si es un no espaciador va a IN
+                        ++cc;
+                        state = IN;
                     break;
-
-                default:   //Si es un no espaciador va a IN
-                    ++cc;
-                    state = IN;
-                break;
-                }
-            case IN:
-            }
-                  
-                }
-
-
-
-
-
-
+                    }
+                case IN:
+                    switch (c) //Si esta en IN y recibe un caracter
+                    {
+                    case(" "||"\n"||"\t"): //Si en IN leo un espaciador.
+                        v[i]=cc;
+                        ++i;
+                        cc=0;
+                    
+                    default: //Si leo un no espaciador.
+                        ++cc;
+                    break;
+                    }
+                }    
         }
-        
     }
-
-
-
-
-
+        
 }
+
+
+
+
+
