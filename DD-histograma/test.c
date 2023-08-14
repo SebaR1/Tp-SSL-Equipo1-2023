08504ed48811702ee9a4ce547fd14ef26041f514
SebastianRomero <romerosebastian781@gmail.com>
2023-08-13 21:09:47 -0300

    parte 2 de correcciones

--- DD-histograma/test.c
+++ DD-histograma/test.c
@@ -2,36 +2,15 @@
 #include "graficador.h"
 #include "histograma.h"
 
-//#define IMPLEMENTACION_1
-//#define IMPLEMENTACION_2
-//#define IMPLEMENTACION_3
-#define IMPLEMENTACION_4
-
 int main ()
 {
     int i=0;
     unsigned cantidadDePalabras=0;
     enum {CANTIDAD_DE_CONTADORES=15};
     unsigned v[CANTIDAD_DE_CONTADORES]={0}; 
+    int flujo = gets()
 
-    
-
-    #ifdef IMPLEMENTACION_1
     cantidadDePalabrasPorSuLongitud (v, CANTIDAD_DE_CONTADORES, stdin);
     dibujarHistograma(v, CANTIDAD_DE_CONTADORES);
-    #endif
-    #ifdef IMPLEMENTACION_2
-    cantidadDePalabrasPorSuLongitud (v, CANTIDAD_DE_CONTADORES, stdin);
-    dibujarHistograma(v, CANTIDAD_DE_CONTADORES);
-    #endif 
-    #ifdef IMPLEMENTACION_3
-    cantidadDePalabrasPorSuLongitud (v, CANTIDAD_DE_CONTADORES, stdin);
-    dibujarHistograma(v, CANTIDAD_DE_CONTADORES);
-    #endif 
-    #ifdef IMPLEMENTACION_4
-    cantidadDePalabrasPorSuLongitud (v, CANTIDAD_DE_CONTADORES, stdin);
-    dibujarHistograma(v, CANTIDAD_DE_CONTADORES);
-    #endif 
-  
 }
 
