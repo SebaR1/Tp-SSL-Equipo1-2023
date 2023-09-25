bb009638251157d4e1fd20b3a907b25614da9ef3
JulianTettamanti <jtettamanti@frba.utn.edu.ar>
2023-09-25 20:32:25 -0300

    Correcciones

--- PolCalc/Scanner.c
+++ PolCalc/Scanner.c
@@ -4,13 +4,10 @@
 #include <stdio.h>
 
 #define MAXTAMANIO 100
-
 bool getNextToken(Token *t){
     char lexeme[MAXTAMANIO];
-
     int c, i, nextChar;
 
-    
     while((lexeme[0] = c = getchar()) == ' ' || lexeme[0] == '\t') //Lee el lexema e ignora tabs y espacios
     lexeme[1] = '\0';
    
@@ -26,33 +23,32 @@ bool getNextToken(Token *t){
             if (lexeme[i] == '.'){
             while (isdigit(lexeme[++i] = getchar())); // Si encuentra un número fraccionario después de un . lo guarda
             }
+            if(!isdigit(lexeme[i]))
+            ungetc(lexeme[i],stdin);
             lexeme[i] = '\0';
             t->type = Number; 
             t->val = atof(lexeme); //Convierte el char en un float
             return true;
         }
 
-        if(lexeme [1] == '\n' && (c = getchar()) == EOF){
+        if((lexeme [1] == '\n' && (c = getchar()) == EOF) || lexeme[1] == '-'){
         ungetc(lexeme[1],stdin);
         }
-        
+
         t->type = lexeme[0];
         t->val = 0;
         return true;
     }
 
     i = 0;
-
- 
     while(isdigit(lexeme[++i] = c = getchar())); // Si encuentra algo que sea un dígito empieza a guardar el número
     if (c == '.'){
         while (isdigit(lexeme[++i] = c = getchar())); // Si encuentra un número fraccionario después de un . lo guarda
     }
-    
+
     lexeme[i] = '\0';
     t->type = Number; 
     t->val = atof(lexeme); //Convierte el char en un float
-
 
     if(c != EOF){
         ungetc(c,stdin);
